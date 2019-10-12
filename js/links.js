96674b9e04c90278fee25df4516b853571abf368
unknown <h.e.ergaster@gmail.com>
2019-10-12 12:33:44 -0700

    links added

--- js/links.js
+++ js/links.js
@@ -3,10 +3,10 @@ document.getElementById("butProj").onclick = function() {
   location.href = "https://cponline.pw/";
 };
 document.getElementById("butLink").onclick = function() {
-  location.href = "https://cponline.pw/";
+  location.href = "https://www.linkedin.com/in/eugenio-bonello-90b946187/";
 };
 document.getElementById("butGit").onclick = function() {
-  location.href = "https://cponline.pw/";
+  location.href = "https://github.com/bcus789";
 };
 document.getElementById("butCont").onclick = function() {
   location.href = "https://cponline.pw/";
