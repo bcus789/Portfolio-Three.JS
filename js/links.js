2a8f47d520a23c7d2993303b617ae077c60dbf11
Charlie NoFace <youremail@gmail.comh.e.ergaster@gmail.comgit config --global user.email youremail@gmail.com>
2019-12-02 16:10:32 -0800

    yes

--- js/links.js
+++ js/links.js
@@ -3,6 +3,7 @@ document.getElementById("butProj").onclick = function() {
   location.href = "https://cponline.pw/";
 };
 document.getElementById("butLink").onclick = function() {
+  console.log("hello")
   location.href = "https://www.linkedin.com/in/eugenio-bonello-90b946187/";
 };
 document.getElementById("butGit").onclick = function() {
